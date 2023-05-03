f373b4dc516a1394f9ebc96d4e55b38cb60a2f63
Efraim Munthe <efrayanglain@gmail.com>
2023-05-03 18:09:33 +0700

    refactor data: single source of truth

--- src/views/ClockView.vue
+++ src/views/ClockView.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { onMounted, ref } from "vue";
+
 import WorldClock from "@/components/WorldClock.vue";
+import DigitalWorldClock from "@/components/DigitalWorldClock.vue";
+
+const hours = ref(0);
+const minutes = ref(0);
+const seconds = ref(0);
+
+function getCurrentTime() {
+  const now = new Date();
+
+  hours.value = now.getHours();
+  minutes.value = now.getMinutes();
+  seconds.value = now.getSeconds();
+}
+
+setInterval(getCurrentTime, 1000);
+
+onMounted(getCurrentTime);
 </script>
 
 <template>
   <main>
-    <WorldClock></WorldClock>
+    <DigitalWorldClock :hours="hours" :minutes="minutes" :seconds="seconds" />
+    <WorldClock :hours="hours" :minutes="minutes" :seconds="seconds" />
   </main>
 </template>
 
