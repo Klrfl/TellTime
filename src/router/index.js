7bc5ad08440b809ff8889faf348775239f135ae2
Efraim Munthe <efrayanglain@gmail.com>
2023-05-03 09:25:52 +0700

    changed routes link and components

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import ClockView from "../views/ClockView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "clock",
+    component: ClockView,
+  },
+  {
+    path: "/timer",
+    name: "timer",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/TimerView.vue"),
+  },
+  {
+    path: "/stopwatch",
+    name: "stopwatch",
+    component: () => import("../views/StopwatchView.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes: routes,
+});
 
-export default router
+export default router;
