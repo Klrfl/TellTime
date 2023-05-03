40c482cba22baee5afb1cfa9fc6622bdcfd0a812
Efraim Munthe <efrayanglain@gmail.com>
2023-05-03 17:40:10 +0700

    digital clock

---
+++ src/components/DigitalWorldClock.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="digital-clock">
+    <div id="hours">{{ hours }}</div>
+    <div id="minutes">{{ minutes }}</div>
+    <div id="seconds">{{ seconds }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  hours: Number,
+  minutes: Number,
+  seconds: Number,
+});
+</script>
+
+<style>
+.watch-display {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+
+.watch-display__displays {
+  outline: 1px solid #aaa;
+  padding: 2rem;
+}
+</style>
