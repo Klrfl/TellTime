831b3daa674adaf9044b92cd27a6753417d25bd0
Efraim Munthe <efrayanglain@gmail.com>
2023-05-05 21:03:05 +0700

    import digital clock and time logic, change style

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="timer-container">{{ hours }} : {{ minutes }} : {{ seconds }}</div>
+  <div class="timer-container">
+    <section class="timer-display">
+      {{ hours }} : {{ minutes }} : {{ seconds }}
+    </section>
+    <section class="timer-button-container">
+      <button class="start-timer">Start timer</button>
+      <button class="end-timer">End timer</button>
+    </section>
+
+    <form class="timer-input">
+      <input type="number" min="0" max="3600" v-model="hours" />
+      <input type="number" min="0" max="60" v-model="minutes" />
+      <input type="number" min="0" max="60" v-model="seconds" />
+      <button @click.prevent="print">print</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
