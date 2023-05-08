310897bfbdb70deacec4c626e61834e2a545a926
Efraim Munthe <efrayanglain@gmail.com>
2023-05-08 18:48:38 +0700

    style tweaks

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,40 @@
 <template>
   <main class="wrapper">
-    <slot name="main-content"></slot>
+    <div class="container">
+      <slot></slot>
+    </div>
+    <div class="container">
+      <slot name="main-content"></slot>
+    </div>
   </main>
 </template>
 
 <style scoped>
 .wrapper {
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.container {
+  padding: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 1 0;
+}
+
+.container:first-child {
+  order: 1;
+}
+
+@media screen and (min-width: 50em) {
+  .wrapper {
+    flex-direction: row;
+  }
+
+  .container:first-child {
+    order: -1;
+  }
 }
 </style>
