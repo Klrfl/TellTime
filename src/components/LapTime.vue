52b0d16de2e8e6fd00b431b96e299e50e420f54b
Efraim Munthe <efrayanglain@gmail.com>
2023-05-09 11:00:56 +0700

    make laptime component

---
+++ src/components/LapTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="laptime-container">
+    <div class="laptime">
+      <div class="lap--number">Lap 1</div>
+      <div class="lap--time">0:00:0</div>
+      <div class="lap--delta">-9304903294</div>
+    </div>
+  </div>
+</template>
+
+<style>
+.laptime {
+  border-bottom: 2px solid #aaa;
+  padding-block: 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 5rem;
+  justify-content: space-between;
+}
+</style>
