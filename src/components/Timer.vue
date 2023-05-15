ada16e41ee21181f4cb40992bde5d98521d1111f
Efraim Munthe <efrayanglain@gmail.com>
2023-05-15 10:48:52 +0700

    initial timer code

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,72 +1,84 @@
 <template>
   <div class="timer">
     <section class="timer-display">
-      {{ hours }} : {{ minutes }} : {{ seconds }}
+      {{ hours }} : {{ minutes }} : {{ seconds }} : {{ milliseconds }}
     </section>
+
     <section class="btn-container">
-      <button class="btn btn--circular" @click="startTimer">
+      <button class="btn btn--circular" @click="startTimer" v-show="isStopped">
         <font-awesome-icon :icon="['fas', 'play']" />
       </button>
-      <button class="btn btn--circular" @click="stopTimer">
+      <button
+        class="btn btn--circular"
+        @click="stopTimer"
+        v-show="isStopped == false">
         <font-awesome-icon :icon="['fas', 'rotate-left']" />
       </button>
     </section>
-
-    <!-- <form class="timer-input">
-      <input type="number" min="0" max="3600" v-model="hours" />
-      <input type="number" min="0" max="60" v-model="minutes" />
-      <input type="number" min="0" max="60" v-model="seconds" />
-      <button @click.prevent="print">print</button>
-    </form> -->
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+import { DateTime } from "luxon";
+
 const hours = ref(0);
 const minutes = ref(0);
 const seconds = ref(0);
+const milliseconds = ref(0);
 
 const isStopped = ref(true);
 
 const startTime = ref(0);
-const currentTime = ref(0);
-const delta = ref(0);
-const targetTime = new Date(35000);
+const deltaFromStartTime = ref(0);
+
+const targetTime = DateTime.fromJSDate(new Date(35000));
+
+const finalDelta = ref(0);
 
 let interval = null;
 
-function getCurrentTime() {
-  return new Date().getTime();
-}
-
 function startTimer() {
-  if (isStopped.value === true) {
-    // startTime.value = startTime.
-  }
-  startTime.value = getCurrentTime();
+  startTime.value = DateTime.utc();
 
   isStopped.value = false;
 
   interval = setInterval(() => {
-    currentTime.value = getCurrentTime();
-    delta.value = new Date(currentTime.value - startTime.value);
+    deltaFromStartTime.value = DateTime.utc().diff(startTime.value, [
+      "hours",
+      "minutes",
+      "seconds",
+      "milliseconds",
+    ]);
 
-    console.log(delta.value);
-    hours.value = delta.value.getHours();
-    minutes.value = delta.value.getMinutes();
-    seconds.value = delta.value.getSeconds();
-  }, 1000);
+    finalDelta.value = targetTime.diff(deltaFromStartTime.value, [
+      "hours",
+      "minutes",
+      "seconds",
+      "milliseconds",
+    ]);
+
+    hours.value = finalDelta.value.hours;
+    minutes.value = finalDelta.value.minutes;
+    seconds.value = finalDelta.value.seconds;
+    milliseconds.value = finalDelta.value.milliseconds;
+  });
 }
 
 function stopTimer() {
   clearInterval(interval);
+  isStopped.value = true;
 }
 </script>
 
 <style>
 .timer-container {
+  text-align: center;
+  font-size: 2rem;
+}
+
+.timer-display {
   text-align: center;
   font-size: 2rem;
 }
