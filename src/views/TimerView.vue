823e3a0de7aca82d3fe85716208a8d9b31b6fa70
Efraim Munthe <efrayanglain@gmail.com>
2023-05-15 11:25:51 +0700

    add targetTime component

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -3,10 +3,33 @@
     <template #main-content>
       <Timer></Timer>
     </template>
+
+    <ul class="target-time-container">
+      <TargetTime
+        v-for="targetTime in targetTimes"
+        :key="targetTime.id"
+        :targetTime="targetTime.time" />
+    </ul>
   </MainLayout>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 import MainLayout from "@/layouts/MainLayout.vue";
 import Timer from "@/components/Timer.vue";
+import TargetTime from "@/components/TargetTime.vue";
+
+const targetTimes = ref([
+  {
+    id: 1,
+    time: 10000,
+  },
+]);
 </script>
+
+<style>
+.target-time-container {
+  list-style: none;
+}
+</style>
