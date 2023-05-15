bf9debeb0bd8ded123fed1117670cb8a50a89a61
Efraim Munthe <efrayanglain@gmail.com>
2023-05-15 11:25:30 +0700

    make component

---
+++ src/components/TargetTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="target-time">
+    <p>{{ targetTime }}</p>
+  </li>
+</template>
+
+<script setup>
+const props = defineProps(["targetTime"]);
+</script>
+
+<style>
+.target-time {
+  outline: 1px solid;
+  padding: 2rem;
+}
+
+.target-time:hover {
+  background: #343434;
+  cursor: pointer;
+}
+</style>
