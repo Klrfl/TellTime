58afd76b8cf9f26e47aad5929cac41dce24600fe
Efraim Munthe <efrayanglain@gmail.com>
2023-06-01 09:35:57 +0700

    change about content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,10 +2,20 @@
   <ContentOnly>
     <h1>About us</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure illum,
-      necessitatibus voluptatibus reprehenderit mollitia vitae officiis ad
-      tenetur, perferendis animi inventore laudantium accusantium nemo incidunt
-      tempore dolores a temporibus repudiandae.
+      TellTime is an application born from a Project-based learning project.
+      This app was made by three Indonesian 10th grade students, which consists
+      of one main developer, (me,
+      <a href="https://github.com/Klrfl" target="_blank">Klrfl</a>), and two
+      designers (<a href="https://github.com/thaaadek">Atha</a> and Devano).
+    </p>
+    <p>
+      This app was not meant to be for "daily" or "real" use, but for us to
+      learn web programming in general; but if you found it useful, thank you so
+      Much! We are sorry for any inconsistencies, bugs or errors within the app
+      (especially on the clock page). If you found any, please make an issue on
+      this app's
+      <a href="https://github.com/klrfl/TellTime" target="_blank">Github repo</a
+      >. PRs and/or any constructive criticism are very much welcomed!
     </p>
   </ContentOnly>
 </template>
