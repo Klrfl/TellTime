45864e152a3f3957bde7a1bf705c32850d0d8ba3
Efraim Munthe <efrayanglain@gmail.com>
2023-06-04 15:32:45 +0700

    add delete button for more intuitive UX

--- src/components/timer/TargetTime.vue
+++ src/components/timer/TargetTime.vue
@@ -1,9 +1,9 @@
 <template>
-  <li
-    class="target-time"
-    @click="selectTargetTime"
-    @dblclick="deleteTargetTime">
+  <li class="target-time" @click="selectTargetTime">
     <span>{{ targetTime.time }}</span>
+    <button class="target-time__close" @click="deleteTargetTime">
+      <font-awesome-icon :icon="['fas', 'square-xmark']"></font-awesome-icon>
+    </button>
   </li>
 </template>
 
@@ -30,9 +30,32 @@ function deleteTargetTime() {
   padding: 1.5rem 1rem;
   user-select: none;
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   &:hover {
     background: var(--target-time-background-hover);
     cursor: pointer;
+  }
+
+  &__close {
+    font-size: 1.25rem;
+  }
+}
+
+@media (hover: hover) {
+  .target-time__close {
+    opacity: 0;
+    transition: opacity 200ms ease;
+
+    &:focus-visible {
+      opacity: 1;
+    }
+  }
+
+  .target-time:hover .target-time__close {
+    opacity: 1;
   }
 }
 </style>
