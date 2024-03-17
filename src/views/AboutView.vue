742c288128fe8fdde4806acde6d325538a3eca62
Efraim Munthe <efrayanglain@gmail.com>
2024-03-17 11:06:35 +0700

    added github link and changed some words

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,14 +6,15 @@
       This app was made by three Indonesian 10th grade students, which consists
       of one main developer, (me,
       <a href="https://github.com/Klrfl" target="_blank">Klrfl</a>), and two
-      designers (<a href="https://github.com/thaaadek">Atha</a> and Devano).
+      designers (<a href="https://github.com/thaaadek" target="_blank">Atha</a>
+      and <a href="github.com/Devano02" target="_blank">Devano</a>).
     </p>
     <p>
-      This app was not meant to be for "daily" or "real" use, but for us to
-      learn web programming in general; but if you found it useful, thank you so
-      Much! We are sorry for any inconsistencies, bugs or errors within the app
-      (especially on the clock page). If you found any, please make an issue on
-      this app's
+      This app was not meant to be for daily or real use, but for us to learn
+      web programming in general; but we still tried to make it as usable as
+      possible. If you found it useful, thank you so much! We are sorry for any
+      inconsistencies, bugs or errors within the app. If you found any, please
+      make an issue on this app's
       <a href="https://github.com/klrfl/TellTime" target="_blank">Github repo</a
       >. PRs and/or any constructive criticism are very much welcomed!
     </p>
